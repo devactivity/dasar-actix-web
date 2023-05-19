78f5e5ea996cbb94553f66a09c54c6f54c9853bc
devactivity <fn.devactivity@gmail.com>
2023-05-20 06:47:03 +0700

    Middlewareactix-web

---
+++ aw_middleware/src/main.rs
@@ -0,0 +1,119 @@
+/* use actix_web::{get, App, HttpServer, HttpResponse, dev::Service, middleware};
+use futures_util::future::FutureExt;
+
+#[get("/")]
+async fn index() -> HttpResponse {
+    HttpResponse::Ok().body("Hello, world!")
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    env_logger::init_from_env(env_logger::Env::new().default_filter_or("info"));
+
+    HttpServer::new(|| {
+        App::new()
+            .wrap(middleware::Logger::default()) // middleware 1
+            .wrap(middleware::Compress::default()) // middleware 2
+            .wrap(middleware::NormalizePath::trim()) // middleware 3
+
+            .wrap_fn(| req, srv | {
+                println!("Hi from start. You requested: {}", req.path());
+
+                srv.call(req).map(| res | {
+                    println!("Hi from response");
+                    res
+                })
+            })
+            .service(index)
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
+} */
+
+
+
+
+
+use std::future::{ready, Ready};
+use actix_web::{
+    web, App, HttpServer, HttpResponse,
+    dev::{forward_ready, Service, ServiceRequest, ServiceResponse, Transform},
+    Error,
+};
+use futures_util::future::LocalBoxFuture;
+
+// There are two steps in middleware processing.
+// 1. Middleware initialization, middleware factory gets called with
+//    next service in chain as parameter.
+// 2. Middleware's call method gets called with normal request.
+pub struct SayHi;
+
+// Middleware factory is `Transform` trait
+// `S` - type of the next service
+// `B` - type of response's body
+impl<S, B> Transform<S, ServiceRequest> for SayHi
+where
+    S: Service<ServiceRequest, Response = ServiceResponse<B>, Error = Error>,
+    S::Future: 'static,
+    B: 'static,
+{
+    type Response = ServiceResponse<B>;
+    type Error = Error;
+    type InitError = ();
+    type Transform = SayHiMiddleware<S>;
+    type Future = Ready<Result<Self::Transform, Self::InitError>>;
+
+    fn new_transform(&self, service: S) -> Self::Future {
+        ready(Ok(SayHiMiddleware { service }))
+    }
+}
+
+pub struct SayHiMiddleware<S> {
+    service: S,
+}
+
+impl<S, B> Service<ServiceRequest> for SayHiMiddleware<S>
+where
+    S: Service<ServiceRequest, Response = ServiceResponse<B>, Error = Error>,
+    S::Future: 'static,
+    B: 'static,
+{
+    type Response = ServiceResponse<B>;
+    type Error = Error;
+    type Future = LocalBoxFuture<'static, Result<Self::Response, Self::Error>>;
+
+    forward_ready!(service);
+
+    fn call(&self, req: ServiceRequest) -> Self::Future {
+        println!("Hi from start. You requested: {}", req.path());
+
+        let fut = self.service.call(req);
+
+        Box::pin(async move {
+            let res = fut.await?;
+
+            println!("Hi from response");
+            Ok(res)
+        })
+    }
+}
+
+
+async fn index() -> HttpResponse {
+    HttpResponse::Ok().body("Hello, world!")
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    env_logger::init_from_env(env_logger::Env::new().default_filter_or("info"));
+
+    HttpServer::new(|| {
+        App::new()
+            .wrap(SayHi)
+            .route("/", web::get().to(index))
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
+}
